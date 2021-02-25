715ff52b95e045f294d13211d2b6f3fd7e7f4225
Samorukov <korneyksam@gmail.com>
2021-02-25 21:27:04 +0700

    V0.2 - Structure of MVC and DB

---
+++ Sibers/DbStuff/DBSibersContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sibers.DbStuff
+{
+    public class DBSibersContext : DbContext
+    {
+        public DBSibersContext(DbContextOptions dbContext) : base(dbContext) { }
+    }
+}
